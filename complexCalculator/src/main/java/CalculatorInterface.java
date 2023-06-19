a7c0a222165df071a61ce0d91a9524582e13f1e0
Riley-study <120293475+Riley-study@users.noreply.github.com>
2023-06-19 16:52:26 +0500

    add complex number calculator and interface

---
+++ complexCalculator/src/main/java/CalculatorInterface.java
@@ -0,0 +1,7 @@
+public interface CalculatorInterface {
+    ComplexNumberCalculator sum(ComplexNumber num2);
+    ComplexNumberCalculator diff(ComplexNumber num2);
+    ComplexNumberCalculator multi(ComplexNumber num2);
+    ComplexNumber getResult();
+
+}
---
+++ complexCalculator/src/main/java/ComplexNumberCalculator.java
@@ -0,0 +1,32 @@
+public class ComplexNumberCalculator implements CalculatorInterface{
+    private ComplexNumber num1;
+
+    public ComplexNumberCalculator(ComplexNumber num1) {
+        this.num1 = num1;
+    }
+
+    public ComplexNumberCalculator sum(ComplexNumber num2) {
+        double a = num1.getA() + num2.getA();
+        double b = num1.getB() + num2.getB();
+        num1 = new ComplexNumber(a,b);
+        return this;
+    }
+
+    public ComplexNumberCalculator diff(ComplexNumber num2) {
+        double a = num1.getA() - num2.getA();
+        double b = num1.getB() - num2.getB();
+        num1 = new ComplexNumber(a,b);
+        return this;
+    }
+
+    public ComplexNumberCalculator multi(ComplexNumber num2) {
+        double a = num1.getA()* num2.getA() - num1.getB()* num2.getB();
+        double b = num1.getA()* num2.getA() - num1.getB()* num2.getB();
+        num1 = new ComplexNumber(a,b);
+        return this;
+    }
+
+    public ComplexNumber getResult() {
+        return num1;
+    }
+}
